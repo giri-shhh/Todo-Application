f37306b619fbf2300aa45967366c5f94f8fbb9a2
Girish <pgirisha77@gmail.com>
2020-03-08 20:15:08 +0530

    Issue fix

--- src/main/java/com/example/todoapplication/controller/UserController.java
+++ src/main/java/com/example/todoapplication/controller/UserController.java
@@ -3,13 +3,11 @@ package com.example.todoapplication.controller;
 import com.example.todoapplication.exception.UserNotFoundException;
 import com.example.todoapplication.model.User;
 import com.example.todoapplication.service.UserService;
-import com.sun.xml.internal.ws.api.pipe.ContentType;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -28,7 +26,7 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping
     public List<User> retrieveAllUsers() {
         return this.userService.getUsers();
     }
