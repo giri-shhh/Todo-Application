8b57bbbf2d0480c6a37030a2dab5d04496bd524b
Girish <pgirisha77@gmail.com>
2020-04-18 02:35:21 +0530

    Refactor unused imports

--- src/main/java/com/example/todoapplication/controller/TodoController.java
+++ src/main/java/com/example/todoapplication/controller/TodoController.java
@@ -1,20 +1,16 @@
 package com.example.todoapplication.controller;
 
+import java.util.List;
+
 import com.example.todoapplication.exception.UserNotFoundException;
-import com.example.todoapplication.model.Product;
 import com.example.todoapplication.model.Todo;
 import com.example.todoapplication.respository.TodoRepository;
 import com.example.todoapplication.respository.UserRepository;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("todos/{userId}")
@@ -30,6 +26,7 @@ public class TodoController {
 
     @GetMapping
     public List<Todo> retrieveAllTodo(@PathVariable Long userId) {
-        return this.todoRepository.findAllByUser(userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("id " + userId)));
+        return this.todoRepository.findAllByUser(
+                userRepository.findById(userId).orElseThrow(() -> new UserNotFoundException("id " + userId)));
     }
 }
